7caa4ffd4542a7b5ad982916fff5665ae9e578ca
Hugo <haaa20@sussex.ac.uk>
2023-03-29 20:47:35 +0100

    Shell code for TftpServer... probably be awhile till I actually make use of it tho lol

--- src/main/java/myTftp/TftpServer.java
+++ src/main/java/myTftp/TftpServer.java
@@ -1,18 +1,35 @@
 package myTftp;
 
+import java.net.DatagramPacket;
+import java.net.SocketAddress;
+import java.util.HashMap;
+
 public class TftpServer extends TftpUser implements Runnable {
+    private boolean running;
+    private HashMap<Integer, SocketAddress> clients;
+    private int nextUniqueId;
+
     public TftpServer(String name, int portNo) {
         super(name, portNo);
+        this.running = false;
+        this.nextUniqueId = 0;
+        this.clients = new HashMap<>();
     }
 
-    private void serverLoop() {
-        while (true) {
-            rawReceive();
+    private void awaitRequests() {
+        while (running) {
+            DatagramPacket request = rawReceive();
+            clients.put(nextUniqueId++, request.getSocketAddress());
         }
     }
 
     @Override
     public void run() {
+        running = true;
+        awaitRequests();
+    }
 
+    public void terminate() {
+        running = false;
     }
 }
