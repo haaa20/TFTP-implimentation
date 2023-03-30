be96e51c37f1b0757fe9dd0e2db5fc7bc36329f8
haaa20 <haaa20@sussex.ac.uk>
2023-03-30 12:23:56 +0100

    Rrq and Wrq packets

--- src/main/java/myTftp/RrqTftpPacket.java
+++ src/main/java/myTftp/RrqTftpPacket.java
@@ -1,12 +1,20 @@
 package myTftp;
 
 public class RrqTftpPacket extends TftpPacket{
-    public RrqTftpPacket() {
+    private byte[] filename;
+
+    public RrqTftpPacket(String filename) {
         super(1);
+        this.filename = filename.getBytes();
     }
 
     @Override
     public byte[] toBytes() {
-        return new byte[0];
+        byte[] asBytes = new byte[filename.length + 4];
+
+        asBytes[0] = (byte) getOpcode();
+        System.arraycopy(filename, 0, asBytes, 2, filename.length);
+
+        return asBytes;
     }
 }
--- src/main/java/myTftp/WrqTftpPacket.java
+++ src/main/java/myTftp/WrqTftpPacket.java
@@ -1,12 +1,20 @@
 package myTftp;
 
 public class WrqTftpPacket extends TftpPacket {
-    public WrqTftpPacket() {
+    private byte[] filename;
+
+    public WrqTftpPacket(String filename) {
         super(2);
+        this.filename = filename.getBytes();
     }
 
     @Override
     public byte[] toBytes() {
-        return new byte[0];
+        byte[] asBytes = new byte[filename.length + 4];
+
+        asBytes[0] = (byte) getOpcode();
+        System.arraycopy(filename, 0, asBytes, 2, filename.length);
+
+        return asBytes;
     }
 }
