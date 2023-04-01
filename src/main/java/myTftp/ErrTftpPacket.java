109544faf95f12f91e9048f45cea335c5fdd2b72
haaa20 <haaa20@sussex.ac.uk>
2023-04-01 12:13:06 +0100

    Added error packet

---
+++ src/main/java/myTftp/ErrTftpPacket.java
@@ -0,0 +1,23 @@
+package myTftp;
+
+public class ErrTftpPacket extends TftpPacket {
+    private int errorCode;
+    private byte[] errorMessage;
+
+    public ErrTftpPacket(int errorCode, String errorMessage) {
+        super(5);
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage.getBytes();
+    }
+
+    @Override
+    public byte[] toBytes() {
+        byte[] asBytes = new byte[TftpUser.TFTP_CAPACITY];
+
+        asBytes[0] = (byte) getOpcode();
+        asBytes[2] = (byte) errorCode;
+        System.arraycopy(errorMessage, 0, asBytes, 4, errorMessage.length);
+
+        return asBytes;
+    }
+}
